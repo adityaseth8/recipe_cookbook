f71d2face46a273bfbf58e7f2ef6f7353e56d55e
adityaseth8 <aseth@ucdavis.edu>
2026-08-15 12:44:04 -0500

    unit test project skeleton

---
+++ src/apps/Cookbook.Web/Cookbook.Web.UnitTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Cookbook.Web.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = 5;
+            Assert.Equal(5, result);
+        }
+    }
+}
